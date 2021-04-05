641641452f9896922c424addb964f69b4860ab46
Lyve <lyve2909+github@gmail.com>
2021-04-05 17:41:26 +0200

    fix wrong bit inititialization in Chameleon peripherals

--- source/ceLib/ceDspPeripherals.cpp
+++ source/ceLib/ceDspPeripherals.cpp
@@ -8,11 +8,11 @@ namespace ceLib
 	{
 		auto& essi = getEssi();
 
-		const dsp56k::TWord cra =	dsp56k::Essi::CRA_WL0 | dsp56k::Essi::CRA_WL1;	// word length 24 bits
-		const dsp56k::TWord crb =	dsp56k::Essi::CRB_TE0 |							// Transmit 0 Enable
-									dsp56k::Essi::CRB_RE |							// Receive Enable
-									dsp56k::Essi::CRB_TIE |							// Enable Transmit Interrupts
-									dsp56k::Essi::CRB_RIE;							// Enable Receive Interrupts
+		const dsp56k::TWord cra =	(1<<dsp56k::Essi::CRA_WL0) | (1<<dsp56k::Essi::CRA_WL1);	// word length 24 bits
+		const dsp56k::TWord crb =	(1<<dsp56k::Essi::CRB_TE0) |								// Transmit 0 Enable
+									(1<<dsp56k::Essi::CRB_RE);/* |								// Receive Enable
+									(1<<dsp56k::Essi::CRB_TIE) |								// Enable Transmit Interrupts
+									(1<<dsp56k::Essi::CRB_RIE);									// Enable Receive Interrupts*/
 
 		// CRB: Enable Receive & Transmit 0, enable receive & transmit interrupts
 		essi.setControlRegisters(dsp56k::Essi::Essi0, cra, crb);
