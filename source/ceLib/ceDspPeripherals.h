459c61e0bded48159491af5ceb0436e0b5ee73a3
Lyve <lyve2909+github@gmail.com>
2021-04-07 23:01:30 +0200

    unused stuff removed

--- source/ceLib/ceDspPeripherals.h
+++ source/ceLib/ceDspPeripherals.h
@@ -2,17 +2,6 @@
 
 #include "../dsp56300/source/dsp56kEmu/peripherals.h"
 
-namespace std
-{
-	class mutex;
-}
-
-namespace dsp56k
-{
-	class DSP;
-	class Essi;
-}
-
 namespace ceLib
 {
 	class DspPeripherals final : public dsp56k::PeripheralsDefault
